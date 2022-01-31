40a4d921f0eef815a1bc5d5548c7050bdc53d9f1
elizabethmroz <elizabethjmroz@gmail.com>
2022-01-31 12:13:00 +0000

    Defining functions

--- NetCDF_reading_plotting.py
+++ NetCDF_reading_plotting.py
@@ -16,4 +16,42 @@ import numpy as np
 import netCDF4 as nc
 import matplotlib.pyplot as plt
 import datetime as dt
-from mpl_toolkits.basemap import Basemap, addcyclic, shiftgrid
+
+
+def ncdump(nc_fid, verb=True):
+    '''
+    ncdump outputs dimensions, variables, and their attribute information.
+    ncdump requires a valid instance of Dataset
+
+    Parameters
+    ----------
+    nc_fid : NetCDF4.dataset
+        DESCRIPTION.
+    verb : Boolean
+        whether or not nc_attrs, nc_dims, and nc_vars are printed
+
+    Returns
+    -------
+    nc_attrs : list
+        A Python list of the NetCDF file global attributes
+    nc_dims : list
+        A Python list of the NetCDF file dimensions
+    nc_vars : list
+        A python list of the NetCDF file variables
+    '''
+    def print_ncattr(key):
+    '''
+    Prints the NetCDF file attributes for a given key
+    
+        Parameters
+        ----------
+        key : unicode
+            a valid netCDF4.dataset.variables key
+
+        Returns
+        -------
+        Print of the NetCDF file attributes for a given key
+
+        '''   
+        try:
+            print "\t\"
