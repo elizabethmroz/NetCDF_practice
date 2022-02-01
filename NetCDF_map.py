54ba0c0db77f71df1184e85c215e179b9e4b4983
elizabethmroz <elizabethjmroz@gmail.com>
2022-02-01 10:10:50 +0000

    Version 1.0
    Takes NetCDF data, looks at metadata, dimensions, and variables. Converts time values into a Python datetime format and can select a specific date to plot precipitation for.
    
    No Cartopy functionality as that section of the tutorial seems to be deprecated (see Troubleshooting branch where the original tutorial data was used and did not work with the code).

--- NetCDF_map.py
+++ NetCDF_map.py
@@ -7,8 +7,8 @@ TUTORIAL FOLLOWED:
 import numpy as np
 import netCDF4 as nc
 from matplotlib import pyplot as plt
-from cartopy import crs as ccrs
 import datetime as dt
+
 
 # set path to file
 ncdata_path = "C:\\Users\\gy16e3m\\Documents\\Python\\NetCDF\\pr_day_GFDL-ESM2M_historical_r1i1p1_EWEMBI_20010101-20051231.nc4"
@@ -29,6 +29,7 @@ ACCESS FILE METADATA
     contained.
 '''
 
+
 for attri in ncdata.ncattrs():
     print(attri)
     print('>>',ncdata.getncattr(attri))
@@ -48,6 +49,7 @@ LOAD DIMENSIONS
     Dimensions are a dictionary.
 '''
 
+
 for dim in ncdata.dimensions:
     print(dim)
 
@@ -61,6 +63,7 @@ LOAD VARIABLES
     Variables are a dictionary.
 '''
 
+
 for var in ncdata.variables:
     print(var)
     
@@ -69,32 +72,40 @@ for var in ncdata.variables:
 CONVERT TIME VALUES INTO A PYTHON DATETIME
 '''
 
+
 print(ncdata.variables['time'])
 
-''' Information derived:
+
+''' 
+Information derived:
         
-        Data stored at float64
-        Data is in days (yyyy/mm/dd)
-        Time is the only dimension
+    Data stored at float64
+    Data is in days (yyyy/mm/dd)
+    Time is the only dimension
         
     As a result, datetime striptime and timedelta functions can convert the floating 
     point values to a list of datetime objects. 
 '''
 
+
 # set starting 'strp' time object
 starttime = dt.datetime.strptime(ncdata.variables['time'].units,
-                                 'days since %Y-%m-%d %H:%M:00')
+                                 'hours since %Y-%m-%d %H:%M:0.0')
+
 
 # pull time data into numpy array, build a list of datetime objects
 time_values = ncdata.variables['time'][:]
 strptimes = [starttime + dt.timedelta(days=i) for i in time_values]
 
+
 # obtain time index for a specific input time
-choice = '2005-04-16 00:00:00' # if choice is beyond the index, the last index is printed
-choice_strp = dt.datetime.strptime(choice,'%Y-%m-%d %H:%M:00')
+choice = '2001-01-30 00:00:0.0' # if choice is beyond the index, the last index is printed
+choice_strp = dt.datetime.strptime(choice,'%Y-%m-%d %H:%M:0.0')
 tidx = np.argmin(np.abs(np.array(strptimes)-choice_strp))
 
+
 print("Index:%i, time:"%tidx, strptimes[tidx])
+
 
 '''
 PLOT THE DATA
@@ -102,55 +113,28 @@ PLOT THE DATA
     lat and lon are 1-dimensional variables
     time is a 1-dimensional variable
     precip is a 3-dimensional variable
-
-
 '''
 
+
 # access variable for each dimension
-precip = ncdata.variables['pr'][tidx,:]
+t2 = ncdata.variables['air'][tidx,:]
 lon = ncdata.variables['lon'][:]
 lat = ncdata.variables['lat'][:]
 
 
-'''TROUBLESHOOTING
-
-    TypeError: draw_wrapper() got an unexpected keyword argument 'inframe'
-    
-    '''
-    
-# plot TEST 1
+# simple plot
 plt.figure(figsize=(10,7))
-ax = plt.subplot(projection = ccrs.Mercator())
-Z = ax.pcolormesh(lon,lat,precip,cmap='jet')
-plt.colorbar(Z)
-ax.coastlines()
-plt.show() # this doesnt work
-
-# plot TEST 2
-plt.figure(figsize = (10,7))
-plt.pcolormesh(lon,lat,precip,cmap='jet')
-
-plt.show() # this works
-
-# plot TEST 3
-plt.figure(figsize=(10,7))
-ax = plt.axes(projection = ccrs.PlateCarree())
-plt.title('Australia')
-ax.set_extent([112, 154, -44, -5.6]), ccrs.PlateCarree()
-ax.coastlines(resolution='110m')
-plt.plot(145.014659, -37.785922,  markersize=2, marker='o', color='red')
+plt.pcolormesh(lon,lat,t2,cmap='jet')
 plt.show()
 
 
-Z = ax.pcolormesh(lon,lat,precip,cmap='jet')
-plt.colorbar(Z)
-ax.coastlines()
-plt.show() # this doesnt work
 
 '''
 NOTES FOR SELF
         The file is from 1st Jan 2001 to 31st Dec 2005
         
+        I have no idea what the units are in for precipitation 
+     
 MISC THINGS COULD BE FUN TO DO
         Could do a for-loop to create a plot for each day, figure out a way of merging them into a gif:
             Potential algorithm =
